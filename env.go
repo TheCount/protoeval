af459e6b254fc45ad9c590c8adfd0349c8fbc85c
Alexander Klauer <Alexander.Klauer@googlemail.com>
2021-10-17 18:18:27 +0200

    Add initial public Env methods

--- env.go
+++ env.go
@@ -1,5 +1,11 @@
 package protoeval
 
+const (
+	// DefaultEvalMax is the default maximum number of sub-evaluations before
+	// a call to Eval is aborted.
+	DefaultEvalMax = 1000
+)
+
 // Env describes an environment within which an evaluation can take place.
 // Instances of this type are not safe for concurrent use. Clone your
 // environment instead.
@@ -13,6 +19,57 @@ type Env struct {
 	// cyclesLeft is the number of cycles (an evaluation cost measure) left
 	// before we abort an evaluation.
 	cyclesLeft int
+}
+
+// NewEnv creates a new, empty environment.
+func NewEnv() *Env {
+	return &Env{
+		values:     make(map[string]interface{}),
+		cyclesLeft: DefaultEvalMax,
+	}
+}
+
+// Set sets a value in this environment under the given key. If there already
+// is a value, it is overwritten. If value is nil, it is deleted instead.
+// This environment is returned
+// FIXME: not all value types can be sensibly used in all contexts, should
+// specify this further.
+func (e *Env) Set(key string, value interface{}) *Env {
+	if value == nil {
+		delete(e.values, key)
+		return e
+	}
+	e.values[key] = value
+	return e
+}
+
+// Get gets a value from this environment for the given key. If no such value
+// exists, ok == false is returned.
+func (e *Env) Get(key string) (value interface{}, ok bool) {
+	value, ok = e.values[key]
+	return
+}
+
+// SetEvalMax sets the maximum number of sub-evaluations for an Eval call
+// with this environment. A non-positive value will cause all evaluations to
+// fail. This environment is returned.
+func (e *Env) SetEvalMax(max int) *Env {
+	e.cyclesLeft = max
+	return e
+}
+
+// Clone creates a copy of this environment.
+// Note that values set with Set or through previous evaluations are copied
+// shallowly.
+func (e *Env) Clone() *Env {
+	result := &Env{
+		values:     make(map[string]interface{}, len(e.values)),
+		cyclesLeft: e.cyclesLeft,
+	}
+	for k, v := range e.values {
+		result.values[k] = v
+	}
+	return result
 }
 
 // shiftScopeByName returns a shallow copy of this environment with the
